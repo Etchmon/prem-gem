5dad7f808af1c57e32f8c0172a0051199203fc18
James Etchells <etch.rush@gmail.com>
2023-06-14 15:48:40 -0700

    modified:   src/pages/api/data.ts

--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -9,7 +9,16 @@ export default async function fantasyData(req: NextApiRequest, res: NextApiRespo
   try {
     const response = await fetch('https://fantasy.premierleague.com/api/bootstrap-static/');
     const data = await response.json();
-    res.status(200).json(data);
+    const playerList = data.elements;
+
+    // Pull Player data out from json
+    // Use players name to scrape transfermarkt data
+    // Create new model for Player Schema
+    // Add data from API and transfermarkt to new player model
+    // Insert player into database
+
+
+    res.status(200).json(playerList);
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Error fetching data' });
